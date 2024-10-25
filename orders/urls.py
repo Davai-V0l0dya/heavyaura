6a5bb4f5cf2dec05ea6ec2bf54209d1051c7fa12
Davai-V0l0dya <147437465+Davai-V0l0dya@users.noreply.github.com>
2024-10-25 21:42:02 +0300

    Comments added

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,12 @@
+# Импортируем функцию path для определения маршрутов URL и представления из текущего приложения.
 from django.urls import path
-from . import views
+from . import views  # Импортируем views из текущего модуля.
 
-
+# Устанавливаем пространство имен для URL-шаблонов приложения 'orders'.
 app_name = 'orders'
 
+# Определяем список маршрутов URL для приложения 'orders'.
 urlpatterns = [
+    # URL для создания нового заказа; обрабатывает путь 'create/'.
     path('create/', views.order_create, name='order_create'),
 ]
