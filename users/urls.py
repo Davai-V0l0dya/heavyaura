866120c2bbe8e46f276a475cf0681cbdd3bbaad0
Davai-V0l0dya <147437465+Davai-V0l0dya@users.noreply.github.com>
2024-10-25 21:37:58 +0300

    Comments added

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
-from . import views
+from django.urls import path  # Импортируем функцию path для определения маршрутов
+from . import views  # Импортируем представления из текущего приложения
 
+app_name = 'users'  # Устанавливаем пространство имен для маршрутов в приложении пользователей
 
-app_name = 'users'
-
+# Определение URL-шаблонов для приложения пользователей
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('registration/', views.registration, name='registration'),
-    path('profile/', views.profile, name='profile'),
-    path('logout/', views.logout, name='logout'),
+    path('login/', views.login, name='login'),  # Маршрут для страницы входа; вызывает представление login
+    path('registration/', views.registration, name='registration'),  # Маршрут для страницы регистрации; вызывает представление registration
+    path('profile/', views.profile, name='profile'),  # Маршрут для страницы профиля пользователя; вызывает представление profile
+    path('logout/', views.logout, name='logout'),  # Маршрут для выхода из аккаунта; вызывает представление logout
 ]
