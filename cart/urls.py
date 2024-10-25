bbfcd69d79542704e834e197bd7fdb572067bd9a
Davai-V0l0dya <147437465+Davai-V0l0dya@users.noreply.github.com>
2024-10-25 21:48:19 +0300

    Comments added

--- cart/urls.py
+++ cart/urls.py
@@ -1,12 +1,19 @@
+# Импортируем функцию path для определения URL-шаблонов.
 from django.urls import path
+# Импортируем представления (views) из текущего приложения.
 from . import views
 
-
+# Устанавливаем пространство имен для URL-шаблонов приложения "cart".
 app_name = 'cart'
 
+# Определяем список URL-шаблонов для приложения корзины.
 urlpatterns = [
+    # URL для отображения деталей корзины. Здесь используется функция представления cart_detail.
     path('', views.cart_detail, name='cart_detail'),
+    
+    # URL для добавления продукта в корзину. Используется параметр product_id из URL.
     path('add/<int:product_id>/', views.cart_add, name='cart_add'),
-    path('remove/<int:product_id>/', views.cart_remove,
-         name='cart_remove'),
+    
+    # URL для удаления продукта из корзины. Используется параметр product_id из URL.
+    path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
 ]
